e3c9a72a6fadd97250b3663c03369e239dd85c1e
shockli <shockli@yandex.ru>
2022-12-20 11:29:19 +0300

    Update StackExample.java

--- StackExample.java
+++ StackExample.java
@@ -13,23 +13,26 @@
                Вывод: 0
                * */
 
+import java.util.ArrayList;
+
 public class StackExample {
     public static void main(String[] args) throws Exception {
-        System.out.println(getRightBrackets("(()"));
         System.out.println(getRightBrackets(")()())"));
         System.out.println(getRightBrackets(")(()())"));
         System.out.println(getRightBrackets(")("));
+        System.out.println(getRightBrackets("(()(()())"));
   }
   public static String getRightBrackets(String test){
         String stack = "";
         String result = "";
         Boolean reduceDeepLength = false;
+        ArrayList<Integer> unclosedIndex = new ArrayList();
         for(int i = 0; i < test.length();){
             if(stack.endsWith("(") && test.substring(i,i+1).equals(")")){
                 String updatedResult = ""; //вставка текущих скобок в result (формируемый ответ)
                 if(reduceDeepLength){
                     //скобки обворачивают предыдуший результат если глубина стека уменьшилась
-                    updatedResult= "(" + result + ")";
+                    updatedResult=result.substring(0,unclosedIndex.get(unclosedIndex.size()-1)) + "(" + result.substring(unclosedIndex.get(unclosedIndex.size()-1))  + ")";
                 }else{
                     //или идут следом
                     updatedResult= result + "()";
@@ -37,10 +40,13 @@ public class StackExample {
                 result = updatedResult;
                 stack= stack.substring(0, stack.length()-1);  //уменьшение глубины стека
                 reduceDeepLength=true;
+                unclosedIndex.remove(unclosedIndex.size()-1);
             }else{
                 stack+= test.substring(i,i+1); //увеличение глубины стека
+                unclosedIndex.add(i);
                 reduceDeepLength=false;
             }
+            //System.out.println(i + " " + stack);
             i++;
         }
       return result.length() + " - " + result;
